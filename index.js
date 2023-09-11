8f18e30c61356bc4e2736c34ee076a150fb5bdfc
ARTHUR1818ARTHUR <144696335+ARTHUR1818ARTHUR@users.noreply.github.com>
2023-09-11 12:35:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const server = express();
+
+server.get('/web2, ( req, res) =>  
+    return res.json( { message: "Hello world" })
+)
+    server.listen(3000);
+
+
+server.get('web2', (req, res) => {
+    return res.json(web2);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
